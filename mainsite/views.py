7ec6dcf4ff07fb9e3bdbf809d3139a40e8767bec
Wei Lyu <lvwei1001@live.com>
2018-01-15 00:50:02 +0800

    Add templates

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import auth
+
 
 # Create your views here.
+def index(request):
+    user = User.objects.get(username='lvwei1001')
+    auth.login(request, user)
+    return render(request, 'base.html', locals())
